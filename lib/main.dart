b3ffa25e49645326fd8440e6085a14af25496df2
asadamatic <asadamatic@gmail.com>
2021-07-10 12:02:26 +0500

    Integrated API calls to the backend.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:digitalpsychologist/Network/api_call.dart';
 import 'package:digitalpsychologist/my_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+
+  ApiCall().getStressLevel();
   runApp(MyApp());
 }
