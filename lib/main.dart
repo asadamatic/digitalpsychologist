c70129750e396aef95f2b64e7a1af2cd7cc9edc9
asadamatic <asadamatic@gmail.com>
2021-07-10 12:04:14 +0500

    Revert "Integrated API calls to the backend."
    This reverts commit b3ffa25e

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:digitalpsychologist/Network/api_call.dart';
 import 'package:digitalpsychologist/my_app.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
-
-  ApiCall().getStressLevel();
+void main() {
   runApp(MyApp());
 }
